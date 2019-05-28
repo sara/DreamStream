220abdba1edf45614ec37c1feede8d558296bf57
Ubuntu <ubuntu@ip-172-31-45-245.us-east-2.compute.internal>
2019-05-28 02:48:08 +0000

    flask stuff

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.login import LoginManager
+from flask_mail import Mail
+from oauth import OAuthSignIn
+
+app = Flask(__name__)
+app.config.from_pyfile('../config.py')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+from views import *
---
+++ run.py
@@ -0,0 +1,13 @@
+import requests
+
+import os
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "hello world"
+
+if __name__ == '__main__':
+    app.run(debug=True)
